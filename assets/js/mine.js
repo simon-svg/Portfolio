d55e83ad9196a6007df9ae44daaea9ad1634e1f9
simon-svg <simonkarapetyan605@gmail.com>
2020-10-28 19:01:10 +0400

    first commit

---
+++ assets/js/mine.js
@@ -0,0 +1,286 @@
+// section education content margines
+const microsoft = document.querySelectorAll(".educ-exper__cont_head_left")[0];
+const college = document.querySelectorAll(".educ-exper__cont_head_left")[1];
+const ugeek = document.querySelectorAll(".educ-exper__cont_head_right")[0];
+const university = document.querySelectorAll(".educ-exper__cont_head_right")[1];
+
+const ugeekHeight = ugeek.clientHeight
+microsoft.style["margin-top"] = ugeekHeight + 30 + "px";
+const microHeight = microsoft.clientHeight
+university.style["margin-top"] = microHeight + 60 + "px";
+const univerHeight = university.clientHeight
+college.style["margin-top"] = univerHeight + 90 + "px";
+
+
+
+// section experience content margines
+const foton = document.querySelectorAll(".educ-exper__cont_head_right")[2];
+const haytech = document.querySelectorAll(".educ-exper__cont_head_left")[2];
+const lucky = document.querySelectorAll(".educ-exper__cont_head_right")[3];
+
+const fotonHeight = foton.clientHeight;
+haytech.style["margin-top"] = fotonHeight + 30 + "px";
+const haytechHeight = haytech.clientHeight;
+lucky.style["margin-top"] = haytechHeight + 60 + "px";
+
+
+
+
+// scrollTop function
+const headerLinks = document.querySelectorAll(".header__link");
+const headerMobileLinks = document.querySelectorAll(".header__mobile-link");
+const homeLogo = document.querySelector(".home__logo");
+window.addEventListener("scroll", () => {
+    const position = window.innerHeight / 1.10;
+
+    if (homeLogo.getBoundingClientRect().top < position) {
+        for (let i = 0; i < headerLinks.length; i++) {
+            headerLinks[i].style.color = "rgb(160, 183, 202)";
+            headerMobileLinks[i].style.color = "rgb(160, 183, 202)";
+        }
+        headerLinks[0].style.color = "#00D646";
+        headerMobileLinks[0].style.color = "#00D646";
+    }
+
+    const homeContLink = document.querySelector(".home__cont_link");
+    if (homeContLink.getBoundingClientRect().top < position) {
+        homeContLink.classList.add("show");
+    }
+
+    const skillsTitle = document.querySelector(".skills__title");
+    if (skillsTitle.getBoundingClientRect().top < position) {
+        skillsTitle.classList.add("show");
+        for (let i = 0; i < headerLinks.length; i++) {
+            headerLinks[i].style.color = "rgb(160, 183, 202)";
+            headerMobileLinks[i].style.color = "rgb(160, 183, 202)";
+        }
+        headerLinks[1].style.color = "#00D646";
+        headerMobileLinks[1].style.color = "#00D646";
+    }
+
+    const educationTitle = document.querySelector(".education__title");
+    if (educationTitle.getBoundingClientRect().top < position) {
+        educationTitle.classList.add("show");
+        for (let i = 0; i < headerLinks.length; i++) {
+            headerLinks[i].style.color = "rgb(160, 183, 202)";
+            headerMobileLinks[i].style.color = "rgb(160, 183, 202)";
+        }
+        headerLinks[2].style.color = "#00D646";
+        headerMobileLinks[2].style.color = "#00D646";
+    }
+
+    const certificateTitle = document.querySelector(".certificate__title");
+    if (certificateTitle.getBoundingClientRect().top < position) {
+        certificateTitle.classList.add("show");
+        for (let i = 0; i < headerLinks.length; i++) {
+            headerLinks[i].style.color = "rgb(160, 183, 202)";
+            headerMobileLinks[i].style.color = "rgb(160, 183, 202)";
+        }
+        headerLinks[3].style.color = "#00D646";
+        headerMobileLinks[3].style.color = "#00D646";
+    }
+
+    const experienceTitle = document.querySelector(".experience__title");
+    if (experienceTitle.getBoundingClientRect().top < position) {
+        experienceTitle.classList.add("show");
+        for (let i = 0; i < headerLinks.length; i++) {
+            headerLinks[i].style.color = "rgb(160, 183, 202)";
+            headerMobileLinks[i].style.color = "rgb(160, 183, 202)";
+        }
+        headerLinks[4].style.color = "#00D646";
+        headerMobileLinks[4].style.color = "#00D646";
+    }
+
+    const projectsTitle = document.querySelector(".projects__title");
+    if (projectsTitle.getBoundingClientRect().top < position) {
+        projectsTitle.classList.add("show");
+        for (let i = 0; i < headerLinks.length; i++) {
+            headerLinks[i].style.color = "rgb(160, 183, 202)";
+            headerMobileLinks[i].style.color = "rgb(160, 183, 202)";
+        }
+        headerLinks[5].style.color = "#00D646";
+        headerMobileLinks[5].style.color = "#00D646";
+    }
+
+    const projectsBot = document.querySelector(".projects__bot");
+    if (projectsBot.getBoundingClientRect().top < position) {
+        projectsBot.classList.add("show");
+    }
+
+    const skillsItemLine = document.querySelectorAll(".skills__item_line");
+    if (skillsItemLine[0].getBoundingClientRect().top < position && window.innerWidth > 450) {
+        skillsItemLine[0].style.width = "50%";
+        setTimeout(() => {
+            skillsItemLine[1].style.width = "45%";
+        }, 50)
+        setTimeout(() => {
+            skillsItemLine[2].style.width = "39%";
+        }, 100)
+        setTimeout(() => {
+            skillsItemLine[3].style.width = "39%";
+        }, 150)
+        setTimeout(() => {
+            skillsItemLine[4].style.width = "45%";
+        }, 200)
+        setTimeout(() => {
+            skillsItemLine[5].style.width = "47%";
+        }, 250)
+        setTimeout(() => {
+            skillsItemLine[6].style.width = "36%";
+        }, 300)
+        setTimeout(() => {
+            skillsItemLine[7].style.width = "39%";
+        }, 350)
+        setTimeout(() => {
+            skillsItemLine[8].style.width = "45%";
+        }, 400)
+    }
+    else if(skillsItemLine[0].getBoundingClientRect().top < position && window.innerWidth <= 450){
+        skillsItemLine[0].style.width = "75%";
+        setTimeout(() => {
+            skillsItemLine[1].style.width = "70%";
+        }, 50)
+        setTimeout(() => {
+            skillsItemLine[2].style.width = "64%";
+        }, 100)
+        setTimeout(() => {
+            skillsItemLine[3].style.width = "64%";
+        }, 150)
+        setTimeout(() => {
+            skillsItemLine[4].style.width = "70%";
+        }, 200)
+        setTimeout(() => {
+            skillsItemLine[5].style.width = "72%";
+        }, 250)
+        setTimeout(() => {
+            skillsItemLine[6].style.width = "61%";
+        }, 300)
+        setTimeout(() => {
+            skillsItemLine[7].style.width = "64%";
+        }, 350)
+        setTimeout(() => {
+            skillsItemLine[8].style.width = "70%";
+        }, 400)
+    }
+
+    const educatExpContHead = document.querySelectorAll(".educ-exper__cont_head")
+    if (educatExpContHead[0].getBoundingClientRect().top < position) {
+        educatExpContHead[0].classList.add("show");
+        setTimeout(() => {
+            educatExpContHead[2].classList.add("show");
+        }, 50)
+        setTimeout(() => {
+            educatExpContHead[1].classList.add("show");
+        }, 100)
+        setTimeout(() => {
+            educatExpContHead[3].classList.add("show");
+        }, 150)
+    }
+
+    if (educatExpContHead[4].getBoundingClientRect().top < position) {
+        educatExpContHead[4].classList.add("show");
+        setTimeout(() => {
+            educatExpContHead[6].classList.add("show");
+        }, 50)
+        setTimeout(() => {
+            educatExpContHead[5].classList.add("show");
+        }, 100)
+    }
+
+    const CertificProjectsItem = document.querySelectorAll(".certific-projects__item");
+    if (CertificProjectsItem[0].getBoundingClientRect().top < position) {
+        CertificProjectsItem[0].classList.add("show");
+        setTimeout(() => {
+            CertificProjectsItem[1].classList.add("show");
+        }, 50)
+    }
+
+    if (CertificProjectsItem[2].getBoundingClientRect().top < position) {
+        CertificProjectsItem[2].classList.add("show");
+        setTimeout(() => {
+            CertificProjectsItem[3].classList.add("show");
+        }, 50)
+        setTimeout(() => {
+            CertificProjectsItem[4].classList.add("show");
+        }, 100)
+        setTimeout(() => {
+            CertificProjectsItem[5].classList.add("show");
+        }, 150)
+    }
+
+    const educExperMobCont = document.querySelectorAll(".educ-exper-mob__cont");
+    if (educExperMobCont[0].getBoundingClientRect().top < position) {
+        educExperMobCont[0].classList.add("show");
+    }
+
+    if (educExperMobCont[1].getBoundingClientRect().top < position) {
+        educExperMobCont[1].classList.add("show");
+    }
+})
+
+
+
+
+
+// education experience mobile slide
+const educExper = document.querySelectorAll(".educ-exper-mob__item_title");
+const educExperInside = document.querySelectorAll(".educ-exper-mob__item_inside");
+const educExperArrow = document.querySelectorAll(".educ-exper-mob__item_arrow");
+const educExperIcon = document.querySelectorAll(".educ-exper-mob__item_icon");
+
+// for education
+for (let i = 0; i < 4; i++) {
+    educExper[i].addEventListener("click", function educExperAdd(e) {
+        e.stopPropagation()
+        for (let j = 0; j < 4; j++) {
+            if (i !== j) {
+                educExperInside[j].style.height = "0";
+                educExper[j].style.backgroundColor = "rgb(62, 65, 68)";
+                educExperArrow[j].style.backgroundColor = "rgb(72, 74, 77)";
+                educExperIcon[j].style.transform = "rotate(0deg)";
+            }
+            else {
+                educExperInside[i].style.height = "110px";
+                educExper[i].style.backgroundColor = "rgb(72, 74, 77)";
+                educExperArrow[i].style.backgroundColor = "rgb(62, 65, 68)";
+                educExperIcon[i].style.transform = "rotate(180deg)";
+            }
+        }
+    })
+}
+
+// for experience
+for (let i = 4; i < educExper.length; i++) {
+    educExper[i].addEventListener("click", function educExperAdd(e) {
+        e.stopPropagation()
+        for (let j = 4; j < educExper.length; j++) {
+            if (i !== j) {
+                educExperInside[j].style.height = "0";
+                educExper[j].style.backgroundColor = "rgb(62, 65, 68)";
+                educExperArrow[j].style.backgroundColor = "rgb(72, 74, 77)";
+                educExperIcon[j].style.transform = "rotate(0deg)";
+            }
+            else {
+                educExperInside[i].style.height = "110px";
+                educExper[i].style.backgroundColor = "rgb(72, 74, 77)";
+                educExperArrow[i].style.backgroundColor = "rgb(62, 65, 68)";
+                educExperIcon[i].style.transform = "rotate(180deg)";
+            }
+        }
+    })
+}
+
+
+
+
+
+
+
+document.addEventListener("click", () => {
+    for (let i = 0; i < educExper.length; i++) {
+        educExperInside[i].style.height = "0";
+        educExper[i].style.backgroundColor = "rgb(62, 65, 68)";
+        educExperArrow[i].style.backgroundColor = "rgb(72, 74, 77)";
+        educExperIcon[i].style.transform = "rotate(0deg)";
+    }
+})
---
+++ assets/js/slides.js
@@ -0,0 +1,118 @@
+const anchors = document.querySelectorAll('a[href*="#"]');
+
+for (let anchor of anchors) {
+    anchor.addEventListener("click", function (event) {
+        event.preventDefault();
+        const blockId = anchor.getAttribute("href");
+        document.querySelector("" + blockId).scrollIntoView({
+            behavior: "smooth",
+            block: "start",
+        })
+    })
+}
+
+
+
+
+// header panel 
+const header = document.querySelector(".header");
+const headerPanel = document.querySelector(".header__panel");
+const headerPanel_icon = document.querySelector(".header__panel_icon");
+const projectsPage = document.querySelector(".projects-page");
+let headerBool = true;
+
+headerPanel.addEventListener("click", () => {
+    if (headerBool) {
+        headerPanel_icon.style.transform = "rotate(180deg)";
+        header.style.height = 0;
+        if (projectsPage) {
+            projectsPage.style["padding-top"] = "0px"
+        }
+    }
+    else {
+        headerPanel_icon.style.transform = "rotate(0deg)";
+        header.style.height = "70px";
+        if (projectsPage) {
+            projectsPage.style["padding-top"] = "70px"
+        }
+    }
+    headerBool = !headerBool
+})
+
+
+
+
+// scroll top
+const scrollTop = document.querySelector(".scroll-top");
+
+window.onscroll = (e) => {
+    const scrollT = document.documentElement.scrollTop;
+    if (scrollT > 0) {
+        scrollTop.style.display = "block";
+    }
+    else {
+        scrollTop.style.display = "none";
+    }
+}
+
+
+
+
+
+
+// burger menu slide
+const headerBurgerMenu = document.querySelector(".header__burger-menu");
+const headertMobileCont = document.querySelector(".headert__mobile-cont");
+const headerBurgerMenuLine = document.querySelectorAll(".header__burger-menu_item");
+let headertBurgerBool = true;
+
+
+function mobileNavOpen(){
+    headertMobileCont.style.height = "auto";
+    headertMobileCont.style.padding = "20px 0";
+    headerBurgerMenuLine[0].style.margin = "0 0 0 0";
+    headerBurgerMenuLine[2].style.margin = "0 0 0 0";
+    headerBurgerMenuLine[0].style.backgroundColor = "#00D646";
+    headerBurgerMenuLine[2].style.backgroundColor = "#00D646";
+    headerBurgerMenuLine[1].style.display = "none";
+    setTimeout(() => {
+        headerBurgerMenuLine[0].style.transform = "rotate(-45deg)";
+        headerBurgerMenuLine[2].style.transform = "rotate(45deg)";
+        headerBurgerMenuLine[2].style.marginTop = "-3px";
+    }, 100)
+}
+
+function mobileNavClose() {
+    headertMobileCont.style.height = "0";
+    headertMobileCont.style.padding = "0";
+    headerBurgerMenuLine[0].style.transform = "rotate(0deg)";
+    headerBurgerMenuLine[2].style.transform = "rotate(0deg)";
+    headerBurgerMenuLine[0].style.backgroundColor = "rgb(160, 183, 202)";
+    headerBurgerMenuLine[2].style.backgroundColor = "rgb(160, 183, 202)";
+    setTimeout(() => {
+        headerBurgerMenuLine[1].style.marginTop = "5px";
+        headerBurgerMenuLine[2].style.marginTop = "5px";
+        headerBurgerMenuLine[1].style.display = "flex";
+    }, 100)
+}
+
+headerBurgerMenu.addEventListener("click", (e) => {
+    e.stopPropagation()
+    if (headertBurgerBool) {
+        mobileNavOpen();
+    }
+    else {
+        mobileNavClose();
+    }
+    headertBurgerBool = !headertBurgerBool;
+})
+
+
+
+
+
+// document onclick
+document.addEventListener("click", () => {
+    mobileNavClose()
+    headertBurgerBool = true;
+})
